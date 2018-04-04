0e257b2ba7f415abdb3443e99f82c11d8801abe9
Mengyao Song <3076117417@qq.com>
2018-04-04 14:13:07 +0800

    nodeJS 模块系统

---
+++ demos/hello.js
@@ -0,0 +1,16 @@
+// exports.world = function(){
+// 	console.log("Hello world !");
+// }
+
+
+function Hello() {
+	var name;
+	this.setName = function(thyName){
+		name = thyName;
+	};
+	this.sayHello = function() {
+		console.log("Hello" + name);
+	};
+}
+
+module.exports = Hello;
---
+++ demos/moduleDemo.js
@@ -0,0 +1,8 @@
+// var hello = require("./hello");
+// hello.world();
+
+
+var Hello = require("./hello");
+hello = new Hello();
+hello.setName("BYVoid");
+hello.sayHello();
